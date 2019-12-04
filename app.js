83a63dee5e326c5123cbd623d503df6916bc2567
a.stepovyi <a.stepovyi@betsoft.com>
2019-12-04 16:25:10 +0200

    test rest

---
+++ app.js
@@ -0,0 +1,38 @@
+const Koa = require('koa');
+const logger = require('koa-logger');
+const Router = require('koa-router');
+const app = new Koa();
+
+// log all events to the terminal
+app.use(logger());
+
+// error handling
+app.use(async (ctx, next) => {
+    try {
+        await next();
+    } catch (err) {
+        ctx.status = err.status || 500;
+        ctx.body = err.message;
+        ctx.app.emit('error', err, ctx);
+    }
+});
+
+// instantiate our new Router
+const router = new Router();
+const dogRouter = new Router({
+    prefix: '/dogs'
+});
+// require our external routes and pass in the router
+require('./routes/basic')({ router });
+require('./routes/dogs')({ dogRouter });
+
+// tells the router to use all the routes that are on the object
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.use(dogRouter.routes());
+app.use(dogRouter.allowedMethods());
+
+// tell the server to listen to events on a specific port
+const server = app.listen(3001);
+module.exports = server;
